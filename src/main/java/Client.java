3c0704c499de00388933646d8266c2cc62d7be7a
Oscar <oshaw@berkeley.edu>
2020-06-04 18:03:01 -0700

    Client: Audio working, video only takes single image

---
+++ src/main/java/Client.java
@@ -0,0 +1,83 @@
+import com.github.sarxos.webcam.ds.buildin.natives.Device;
+import com.github.sarxos.webcam.ds.buildin.natives.OpenIMAJGrabber;
+import org.bridj.Pointer;
+
+import javax.sound.sampled.*;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.color.ColorSpace;
+import java.awt.image.*;
+import java.io.ByteArrayOutputStream;
+import java.nio.ByteBuffer;
+
+public class Client {
+    public static void main(String[] arguments) throws LineUnavailableException {
+        // Audio format
+        AudioFormat audioFormat = new AudioFormat(8000.0f, 16, 1, true, true);
+        // Microphone, speaker
+        TargetDataLine microphone = (TargetDataLine) AudioSystem.getLine(new DataLine.Info(TargetDataLine.class, audioFormat));
+        SourceDataLine speakers = (SourceDataLine) AudioSystem.getLine(new DataLine.Info(SourceDataLine.class, audioFormat));
+        microphone.open(audioFormat);
+        microphone.start();
+        speakers.open(audioFormat);
+        speakers.start();
+        // Sample
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        int bytesReadTotal = 0;
+        int bytesReadNew;
+        byte[] byteArray = new byte[microphone.getBufferSize() / 5];
+        while (bytesReadTotal < 1000000) {
+            bytesReadNew = microphone.read(byteArray, 0, 1024);
+            bytesReadTotal += bytesReadNew;
+            byteArrayOutputStream.write(byteArray, 0, bytesReadNew);
+            speakers.write(byteArray, 0, bytesReadNew);
+        }
+        microphone.close();
+        speakers.drain();
+        speakers.close();
+    }
+//    public static void main(String[] arguments) {
+//        // Webcam
+//        Dimension dimension = new Dimension(320, 240);
+//        OpenIMAJGrabber openIMAJGrabber = new OpenIMAJGrabber();
+//        Pointer<Device> pointerDevice = openIMAJGrabber.getVideoDevices().get().getDevice(0);
+//        openIMAJGrabber.startSession((int) dimension.getWidth(), (int) dimension.getHeight(), 50, pointerDevice);
+//        // Image
+//        Pointer<Byte> pointerImage = openIMAJGrabber.getImage();
+//        ByteBuffer byteBuffer = pointerImage.getByteBuffer((int) (dimension.getWidth() * dimension.getHeight() * 3));
+//        int[] bytesInEachPixel = {8, 8, 8};
+//        int[] rgbOffsets = {0, 1, 2};
+//        ColorModel colorModel = new ComponentColorModel(
+//            ColorSpace.getInstance(ColorSpace.CS_sRGB),
+//            bytesInEachPixel,
+//            false,
+//            false,
+//            Transparency.OPAQUE,
+//            DataBuffer.TYPE_BYTE
+//        );
+//        ComponentSampleModel componentSampleModel = new ComponentSampleModel(
+//            DataBuffer.TYPE_BYTE,
+//            (int) dimension.getWidth(),
+//            (int) dimension.getHeight(),
+//            3,
+//            (int) dimension.getWidth() * 3,
+//            rgbOffsets
+//        );
+//        byte[] byteArray = new byte[(int) (dimension.getWidth() * dimension.getHeight() * 3)];
+//        byte[][] byteMatrix = new byte[][] { byteArray };
+//        byteBuffer.get(byteArray);
+//        int[] imageOffset = {0};
+//        DataBufferByte dataBufferByte = new DataBufferByte(byteMatrix, byteArray.length, imageOffset);
+//        WritableRaster writableRaster = Raster.createWritableRaster(componentSampleModel, dataBufferByte, null);
+//        BufferedImage bufferedImage = new BufferedImage(colorModel, writableRaster, false, null);
+//        bufferedImage.flush();
+//        // Display image
+//        JFrame jFrame = new JFrame();
+//        jFrame.setSize(dimension);
+//        jFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+//        JLabel jLabel = new JLabel();
+//        jLabel.setIcon(new ImageIcon(bufferedImage));
+//        jFrame.getContentPane().add(jLabel);
+//        jFrame.setVisible(true);
+//    }
+}
