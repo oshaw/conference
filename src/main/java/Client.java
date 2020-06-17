3e452c05dcc83b52a17e810bb7e2c403c3a7dbed
Oscar <oshaw@berkeley.edu>
2020-06-17 13:08:26 -0700

    Untested. Replace ring buffer with broadcast. Metadata encoding incomplete. Packet: Remove. Metadata: Add

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -4,7 +4,6 @@ import io.aeron.Publication;
 import io.aeron.Subscription;
 import io.aeron.driver.MediaDriver;
 import io.aeron.logbuffer.FragmentHandler;
-import io.aeron.logbuffer.Header;
 import org.agrona.BitUtil;
 import org.agrona.DirectBuffer;
 import org.agrona.collections.Long2ObjectHashMap;
@@ -32,51 +31,33 @@ import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicLong;
 
-interface Factory<T> {
-    T create();
-}
+class Metadata {
+    public static final byte SIZE = 8;
+    public static final byte TYPE_ALL = -1;
+    public static final byte TYPE_AUDIO = 0;
+    public static final byte TYPE_VIDEO = 1;
+    public static final byte ORIGIN_LOCAL = 0;
+    public static final byte ORIGIN_FOREIGN = 1;
 
-class Packet {
-    public static final byte SIZE_HEAD = 1 + 4 + 4;
-    public static final byte TYPE_ALL = 0;
-    public static final byte TYPE_AUDIO = 1;
-    public static final byte TYPE_VIDEO = 2;
-
-    public UnsafeBuffer head;
-    public UnsafeBuffer body;
-    private int bodyLoadedLength;
-
-    public Packet() {
-        head = new UnsafeBuffer(new byte[SIZE_HEAD]);
-        setStreamId(0);
-        setSessionId(0);
+    private static final long MASK_TYPE = (long) 1 << (SIZE * 8 - 1);
+    private static final long MASK_ORIGIN = (long) 1 << (SIZE * 8) - 1;
+    private static final long MASK_SESSION_ID = (long) Integer.MAX_VALUE << (SIZE * 8) - 3;
+    
+    public static void setType(UnsafeBuffer buffer, byte type) {
+        byte metadata = buffer.getByte(buffer.capacity() - 1);
+        if (type == TYPE_AUDIO) {
+            buffer.putByte(buffer.capacity() - 1, (byte) (metadata & ~MASK_TYPE));
+            return;
+        }
+        buffer.putByte(buffer.capacity() - 1, (byte) (metadata | MASK_TYPE));
+    }
+    
+    public static void setOrigin(UnsafeBuffer buffer, byte origin) {
         
-        body = new UnsafeBuffer();
-        bodyLoadedLength = -1;
     }
-    
-    public void load(DirectBuffer directBuffer, int offset, int length, Header header) {
-        directBuffer.getBytes(offset, head, 0, SIZE_HEAD);
-        setStreamId(header.streamId());
-        setSessionId(header.sessionId());
 
-        bodyLoadedLength = length - SIZE_HEAD;
-        body = new UnsafeBuffer(new byte[bodyLoadedLength]);
-        directBuffer.getBytes(SIZE_HEAD, body, 0, bodyLoadedLength);
-    }
-    
-    public int bodyLength() { return (bodyLoadedLength != -1) ? bodyLoadedLength : body.capacity(); }
-    public byte type() { return head.getByte(0); }
-    public int streamId() { return head.getInt(1); }
-    public int sessionId() { return head.getInt(1 + 4); }
-    
-    public void setType(byte type) { head.putByte(0, type); }
-    public void setStreamId(int streamId) { head.putInt(1, streamId); }
-    public void setSessionId(int sessionId) { head.putInt(1 + 4, sessionId); }
-
-    public static FactoryPacket factoryPacket = new FactoryPacket();
-    static class FactoryPacket implements Factory<Packet> {
-        @Override public Packet create() { return new Packet(); }
+    public static byte type(DirectBuffer buffer) {
+        return (byte) (buffer.getByte(buffer.capacity() - 1) & MASK_TYPE >> 7);
     }
 }
 
@@ -101,7 +82,7 @@ class BlockingBroadcastTransmitter extends BroadcastTransmitter {
     public AtomicBuffer buffer() { return buffer; }
     
     public void reportReceiveNext(long ticket) {
-        receiverTicketToCursor.put(ticket, calculateNextCursor(receiverTicketToCursor.get(ticket)));
+        receiverTicketToCursor.put(ticket, calculateCursorNext(receiverTicketToCursor.get(ticket)));
     }
     
     @Override public void transmit(int messageTypeId, DirectBuffer sourceBuffer, int sourceIndex, int length) {
@@ -114,7 +95,7 @@ class BlockingBroadcastTransmitter extends BroadcastTransmitter {
         return buffer.getLongVolatile(capacity() + BroadcastBufferDescriptor.LATEST_COUNTER_OFFSET);
     }
     
-    private long calculateNextCursor(long cursor) {
+    private long calculateCursorNext(long cursor) {
         return (long) BitUtil.align(buffer.getInt(RecordDescriptor.lengthOffset((int) cursor & capacity() - 1)), 8);
     }
     
@@ -141,151 +122,194 @@ class BlockingBroadcastReceiver extends BroadcastReceiver {
     }
 }
 
-abstract class Publisher {
+abstract class Producer {
     public final BlockingBroadcastTransmitter transmitter = new BlockingBroadcastTransmitter(new UnsafeBuffer());
+    public final byte type;
+    private final Timer timer;
+    private final Thread thread;
+    
+    Producer(byte type, int delay) {
+        this.type = type;
+        if (delay > 0) {
+            timer = new Timer(delay, (ActionEvent actionEvent) -> produce());
+            thread = null;
+            return;
+        }
+        timer = null;
+        thread = new Thread(() -> { while (true) produce(); });
+    }
+    
+    protected void start() {
+        if (timer != null) {
+            timer.start();
+            return;
+        }
+        thread.start();
+    }
+    
+    protected abstract void produce();
 }
 
-abstract class Subscriber {
+abstract class Consumer {
     private final Set<BlockingBroadcastReceiver> receivers = ConcurrentHashMap.newKeySet();
+    private final byte type;
+    private final Timer timer;
+    private final Thread thread;
 
-    abstract byte packetType();
-
-    abstract void take(Packet packet);
-
-    public void subscribe(Publisher publisher) {
-        receivers.add(new BlockingBroadcastReceiver(publisher.transmitter));
+    Consumer(byte type, int delay) {
+        this.type = type;
+        if (delay != 0) {
+            timer = new Timer(delay, (ActionEvent actionEvent) -> run());
+            thread = null;
+            return;
+        }
+        timer = null;
+        thread = new Thread(() -> { while (true) run(); });
+    }
+    
+    public void subscribe(Producer producer) {
+        receivers.add(new BlockingBroadcastReceiver(producer.transmitter));
+    }
+    
+    private void run() {
+        for (BlockingBroadcastReceiver receiver : receivers) {
+            if (receiver.receiveNext() && (type == Metadata.TYPE_ALL || Metadata.type(receiver.buffer()) == type)) {
+                consume(receiver.buffer(), receiver.offset(), receiver.length());
+            }
+        }
     }
 
     protected void start() {
-        new Thread(() -> {
-            while (true) {
-                for (BlockingBroadcastReceiver receiver : receivers) {
-                    if (receiver.receiveNext()) take(receiver.buffer(), receiver.offset(), receiver.length());
-                }
-            }
-        }).start();
-    }
-}
-
-class Camera extends Publisher {
-    public Camera(Dimension dimension) throws VideoCaptureException {
-        VideoCapture videoCapture = new VideoCapture((int) dimension.getWidth(), (int) dimension.getHeight());
-        new Timer(1000 / 30, (ActionEvent actionEvent) -> {
-            Packet packet = ringBuffer.claim();
-            packet.setType(Packet.TYPE_VIDEO);
-            
-            BufferedImage bufferedImage = new BufferedImage((int) dimension.getWidth(), (int) dimension.getHeight(), BufferedImage.TYPE_INT_ARGB);
-            ImageUtilities.createBufferedImage(videoCapture.getNextFrame(), bufferedImage);
-            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-            try { ImageIO.write(bufferedImage, "png", byteArrayOutputStream); }
-            catch (Exception exception) { exception.printStackTrace(); }
-            
-//            byte metadata = (byte) 0b10000000;
-//            byteArrayOutputStream.write(metadata);
-//            UnsafeBuffer unsafeBuffer = new UnsafeBuffer(byteArrayOutputStream.toByteArray());
-//            BufferedImage bufferedImageOutput = null;
-//            try { bufferedImageOutput = ImageIO.read(new DirectBufferInputStream(unsafeBuffer, 0, unsafeBuffer.capacity() - 1)); }
-//            catch (IOException exception) { exception.printStackTrace(); }
-            
-            packet.body.wrap(byteArrayOutputStream.toByteArray());
-            
-            ringBuffer.commit();
-        }).start();
-    }
-}
-
-class Microphone extends Publisher {
-    public Microphone(AudioFormat audioFormat) throws LineUnavailableException {
-        TargetDataLine targetDataLine = (TargetDataLine) AudioSystem.getLine(new DataLine.Info(TargetDataLine.class, audioFormat));
-        targetDataLine.open(audioFormat);
-        targetDataLine.start();
-        new Timer(1000 / 30, (ActionEvent actionEvent) -> {
-            byte[] bytes = new byte[targetDataLine.available() + 1];
-            targetDataLine.read(bytes, 0, bytes.length - 1);
-            transmitter.transmit(1, new UnsafeBuffer(bytes), 0, bytes.length);
-        }).start();
-    }
-}
-
-class Receiver extends Publisher {
-    private final String address;
-    private final Aeron aeron;
-    private Subscription subscription;
-
-    public Receiver(Aeron aeron, String address) {
-        this.aeron = aeron;
-        this.address = address;
-        subscription = aeron.addSubscription("aeron:udp?endpoint=" + address, 1);
-        new Thread(() -> {
-            FragmentHandler fragmentHandler = (buffer, offset, length, header) -> {
-                ringBuffer.claim().load(buffer, offset, length, header);
-                ringBuffer.commit();
-            };
-            FragmentAssembler fragmentAssembler = new FragmentAssembler(fragmentHandler, 0, true);
-            while (true) {
-                subscription.poll(fragmentAssembler, 100);
-                if (subscription.hasNoImages()) reconnect();
-            }
-        }).start();
-    }
-
-    private void reconnect() {
-        subscription.close();
-        subscription = aeron.addSubscription("aeron:udp?endpoint=" + address, 1);
-        try {
-            Thread.sleep(1000);
-        } catch (Exception exception) {
-            exception.printStackTrace();
+        if (timer != null) {
+            timer.start();
+            return;
         }
+        thread.start();
     }
+
+    protected abstract void consume(DirectBuffer buffer, int offset, int length);
 }
 
-class Sender extends Subscriber {
-    private final Publication publication;
-
-    public Sender(Aeron aeron) {
-        publication = aeron.addPublication("aeron:udp?control-mode=manual", 1);
+class Camera extends Producer {
+    Dimension dimension;
+    VideoCapture videoCapture;
+    
+    public Camera(Dimension dimension) throws VideoCaptureException {
+        super(Metadata.TYPE_VIDEO, 1000 / 30);
+        this.dimension = dimension;
+        videoCapture = new VideoCapture((int) dimension.getWidth(), (int) dimension.getHeight());
         start();
     }
 
-    @Override byte packetType() { return Packet.TYPE_ALL; }
+    @Override protected void produce() {
+        final BufferedImage bufferedImage;
+        final ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        
+        bufferedImage = new BufferedImage((int) dimension.getWidth(), (int) dimension.getHeight(), BufferedImage.TYPE_INT_ARGB);
+        ImageUtilities.createBufferedImage(videoCapture.getNextFrame(), bufferedImage);
+        try { ImageIO.write(bufferedImage, "png", byteArrayOutputStream); }
+        catch (Exception exception) { exception.printStackTrace(); }
+        byteArrayOutputStream.write(0);
+        
+        final UnsafeBuffer buffer = new UnsafeBuffer(byteArrayOutputStream.toByteArray());
+        Metadata.setType(buffer, type);
+        Metadata.setOrigin(buffer, Metadata.ORIGIN_LOCAL);
+        transmitter.transmit(0, buffer, 0, buffer.capacity());
+    }
+}
 
-    @Override public void take(Packet packet) {
-        long outcome = publication.offer(packet.head, 0, packet.head.capacity(), packet.body, 0, packet.body.capacity());
-        // if (outcome < 0) System.out.println(outcome);
+class Microphone extends Producer {
+    TargetDataLine targetDataLine;
+    
+    public Microphone(AudioFormat audioFormat) throws LineUnavailableException {
+        super(Metadata.TYPE_AUDIO, 1000 / 30);
+        targetDataLine = (TargetDataLine) AudioSystem.getLine(new DataLine.Info(TargetDataLine.class, audioFormat));
+        targetDataLine.open(audioFormat);
+        targetDataLine.start();
+    }
+
+    @Override protected void produce() {
+        final UnsafeBuffer buffer = new UnsafeBuffer(new byte[targetDataLine.available() + 1]);
+        targetDataLine.read(buffer.byteArray(), 0, buffer.capacity() - 1);
+        Metadata.setType(buffer, type);
+        Metadata.setOrigin(buffer, Metadata.ORIGIN_LOCAL);
+        transmitter.transmit(0, buffer, 0, buffer.capacity());
+    }
+}
+
+class Sender extends Consumer {
+    private final Publication publication;
+
+    public Sender(Aeron aeron) {
+        super(Metadata.TYPE_ALL, 0);
+        publication = aeron.addPublication("aeron:udp?control-mode=manual", 1);
+        start();
     }
 
     public void addDestination(String address) {
         publication.addDestination("aeron:udp?endpoint=" + address);
     }
+
+    @Override protected void consume(DirectBuffer buffer, int offset, int length) {
+        final long outcome = publication.offer(buffer, offset, length);
+        // if (outcome < 0) System.out.println(outcome);
+    }
 }
 
-class Speaker extends Subscriber {
-    private final SourceDataLine sourceDataLine;
+class Receiver extends Producer {
+    private final Aeron aeron;
+    private final String address;
+    private final FragmentAssembler fragmentAssembler;
+    private Subscription subscription;
+    
+    public Receiver(Aeron aeron, String address) {
+        super(Metadata.TYPE_ALL, 0);
+        this.aeron = aeron;
+        this.address = address;
+        FragmentHandler fragmentHandler = (buffer, offset, length, header) -> {
+            Metadata.setOrigin(buffer, Metadata.ORIGIN_FOREIGN);
+            Metadata.setSessionId(buffer, header.sessionId());
+            transmitter.transmit(Metadata.type(buffer), buffer, offset, length);
+        };
+        fragmentAssembler = new FragmentAssembler(fragmentHandler, 0, true);
+        subscription = aeron.addSubscription("aeron:udp?endpoint=" + address, 1);
+        start();
+    }
+    
+    private void reconnect() {
+        subscription.close();
+        subscription = aeron.addSubscription("aeron:udp?endpoint=" + address, 1);
+        try { Thread.sleep(1000); } catch (Exception exception) { exception.printStackTrace(); }
+    }
+    
+    @Override protected void produce() {
+        subscription.poll(fragmentAssembler, 100);
+        if (subscription.hasNoImages()) reconnect();
+    }
+}
 
+class Speaker extends Consumer {
+    private final SourceDataLine sourceDataLine;
+    
     public Speaker(AudioFormat audioFormat) throws LineUnavailableException {
+        super(Metadata.TYPE_AUDIO, 0);
         sourceDataLine = (SourceDataLine) AudioSystem.getLine(new DataLine.Info(SourceDataLine.class, audioFormat));
         sourceDataLine.open(audioFormat);
         sourceDataLine.start();
         start();
     }
-
-    @Override byte packetType() { return Packet.TYPE_AUDIO; }
-
-    @Override public void take(Packet packet) {
-        byte[] bytes = new byte[packet.bodyLength()];
-        packet.body.getBytes(0, bytes);
-        sourceDataLine.write(bytes, 0, bytes.length);
+    
+    @Override protected void consume(DirectBuffer buffer, int offset, int length) {
+        sourceDataLine.write(buffer.byteArray(), offset, length - Metadata.SIZE);
     }
 }
 
-class Window extends Subscriber {
+class Window extends Consumer {
     private final JFrame jFrame = new JFrame();
-    private JLabel jLabel;
-    private long id;
     private final Long2ObjectHashMap<JLabel> idToJLabel = new Long2ObjectHashMap<>();
 
     public Window(Dimension dimension, String address) {
+        super(Metadata.TYPE_VIDEO, 0);
         jFrame.setLayout(new GridLayout(1, 1));
         jFrame.setSize((int) dimension.getWidth() * 3, (int) dimension.getHeight());
         jFrame.setTitle(address);
@@ -293,26 +317,19 @@ class Window extends Subscriber {
         start();
     }
 
-    @Override byte packetType() { return Packet.TYPE_VIDEO; }
+    @Override protected void consume(DirectBuffer buffer, int offset, int length) {
+        final BufferedImage bufferedImage;
+        try { bufferedImage = ImageIO.read(new DirectBufferInputStream(buffer, offset, length - Metadata.SIZE)); }
+        catch (IOException exception) { return; }
+        if (bufferedImage == null) return;
 
-    @Override public void take(Packet packet) {
-        BufferedImage bufferedImage = null;
-        try {
-            bufferedImage = ImageIO.read(new DirectBufferInputStream(packet.body));
-            if (bufferedImage == null) return;
-        } catch (IOException exception) {
-            // exception.printStackTrace();
-            return;
-        }
-        id = ((long) packet.streamId()) << 32 + (long) packet.sessionId();
+        final long id = Metadata.sessionId(buffer) << 1 | Metadata.origin(buffer);
         if (!idToJLabel.containsKey(id)) {
-            jLabel = new JLabel();
-            jLabel.setIcon(new ImageIcon());
-            jFrame.getContentPane().add(jLabel);
-            idToJLabel.put(id, jLabel);
+            idToJLabel.put(id, new JLabel());
+            idToJLabel.get(id).setIcon(new ImageIcon());
+            jFrame.getContentPane().add(idToJLabel.get(id));
         }
-        jLabel = idToJLabel.get(id);
-        ((ImageIcon) jLabel.getIcon()).setImage(bufferedImage);
+        ((ImageIcon) idToJLabel.get(id).getIcon()).setImage(bufferedImage);
         jFrame.revalidate();
         jFrame.repaint();
     }
